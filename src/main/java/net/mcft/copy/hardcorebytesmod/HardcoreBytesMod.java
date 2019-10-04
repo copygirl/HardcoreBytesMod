7c1d0e3c306bbe08500645f872b92f9c6f5e125c
copygirl <copygirl@mcft.net>
2019-10-04 18:04:34 +0200

    Remove proxies
    Not sure if this is safe ...

--- src/main/java/net/mcft/copy/hardcorebytesmod/HardcoreBytesMod.java
+++ src/main/java/net/mcft/copy/hardcorebytesmod/HardcoreBytesMod.java
@@ -1,17 +1,24 @@
 package net.mcft.copy.hardcorebytesmod;
 
 import net.mcft.copy.hardcorebytesmod.content.HBItems;
-import net.mcft.copy.hardcorebytesmod.proxy.CommonProxy;
+import net.mcft.copy.hardcorebytesmod.item.ItemWorkKnapping.KnappingHandler;
 
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 import org.apache.logging.log4j.Logger;
 
@@ -26,34 +33,51 @@ public class HardcoreBytesMod {
 
     public static Logger LOG;
 
-    @SidedProxy(clientSide = "net.mcft.copy.hardcorebytesmod.proxy.ClientProxy",
-                serverSide = "net.mcft.copy.hardcorebytesmod.proxy.CommonProxy")
-	public static CommonProxy PROXY;
+    // Feature Handlers
+    public static KnappingHandler KNAPPING;
 
+    // Content Classes
     public static HBItems ITEMS;
 
+
     public static CreativeTabs CREATIVE_TAB = new CreativeTabs(MODID) {
-        @Override public ItemStack getTabIconItem() {
-            return new ItemStack(ITEMS.PRIMITIVE_FLINT_KNIFE); } };
+        @Override
+        public ItemStack getTabIconItem()
+            { return new ItemStack(ITEMS.PRIMITIVE_FLINT_KNIFE); }
+    };
 
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event) {
         LOG = event.getModLog();
 
+        KNAPPING = new KnappingHandler();
+
         ITEMS = new HBItems();
 
-        PROXY.preInit();
+        MinecraftForge.EVENT_BUS.register(this);
+        MinecraftForge.EVENT_BUS.register(HardcoreBytesMod.ITEMS);
+        MinecraftForge.EVENT_BUS.register(HardcoreBytesMod.KNAPPING);
+
+        HardcoreBytesMod.ITEMS.initialize();
     }
 
     @EventHandler
     public void init(FMLInitializationEvent event) {
-        PROXY.init();
+
     }
 
     @EventHandler
     public void postInit(FMLPostInitializationEvent event) {
-        PROXY.postInit();
+
+    }
+
+    @SubscribeEvent
+    @SideOnly(Side.CLIENT)
+    public void onRegisterModels(ModelRegistryEvent event) {
+        for (Item item : ITEMS.ALL_ITEMS)
+            ModelLoader.setCustomModelResourceLocation(item, 0,
+                new ModelResourceLocation(item.getRegistryName(), "inventory"));
     }
 
 }
